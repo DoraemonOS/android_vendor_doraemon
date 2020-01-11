2474ea9b4619a19b330f33b0d3b9537bc38c3214
Doraemon-Developer <Doraemon.Operating.System@gmail.com>
2020-01-11 14:07:57 +0000

    Pixel Experience

--- config/branding.mk
+++ config/branding.mk
@@ -1,5 +1,5 @@
 # Set all versions
-CUSTOM_BUILD_TYPE ?= UNOFFICIAL
+CUSTOM_BUILD_TYPE ?= Alpha
 
 CUSTOM_DATE_YEAR := $(shell date -u +%Y)
 CUSTOM_DATE_MONTH := $(shell date -u +%m)
@@ -9,21 +9,23 @@ CUSTOM_DATE_MINUTE := $(shell date -u +%M)
 CUSTOM_BUILD_DATE_UTC := $(shell date -d '$(CUSTOM_DATE_YEAR)-$(CUSTOM_DATE_MONTH)-$(CUSTOM_DATE_DAY) $(CUSTOM_DATE_HOUR):$(CUSTOM_DATE_MINUTE) UTC' +%s)
 CUSTOM_BUILD_DATE := $(CUSTOM_DATE_YEAR)$(CUSTOM_DATE_MONTH)$(CUSTOM_DATE_DAY)-$(CUSTOM_DATE_HOUR)$(CUSTOM_DATE_MINUTE)
 
-CUSTOM_PLATFORM_VERSION := 10.0
+CUSTOM_PLATFORM_VERSION := Quiche
 
-TARGET_PRODUCT_SHORT := $(subst aosp_,,$(CUSTOM_BUILD))
+CUSTOM_DORAEMON_VERSION := v0.1
 
-CUSTOM_VERSION := PixelExperience_$(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)-$(CUSTOM_BUILD_TYPE)
+TARGET_PRODUCT_SHORT := $(subst doraemon_,,$(CUSTOM_BUILD))
+
+CUSTOM_VERSION := DoraemonOS_$(CUSTOM_BUILD)-$(CUSTOM_DORAEMON_VERSION)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_TYPE)
 CUSTOM_VERSION_PROP := 10
-ROM_FINGERPRINT := PixelExperience/$(CUSTOM_PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(CUSTOM_BUILD_DATE)
+ROM_FINGERPRINT := DoraemonOS/$(CUSTOM_PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(CUSTOM_BUILD_DATE)
 
 CUSTOM_PROPERTIES := \
-    org.pixelexperience.version=$(CUSTOM_VERSION_PROP) \
-    org.pixelexperience.version.display=$(CUSTOM_VERSION) \
-    org.pixelexperience.build_date=$(CUSTOM_BUILD_DATE) \
-    org.pixelexperience.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
-    org.pixelexperience.build_type=$(CUSTOM_BUILD_TYPE) \
-    org.pixelexperience.fingerprint=$(ROM_FINGERPRINT)
+    ro.doraemon.version=$(CUSTOM_VERSION_PROP) \
+    ro.doraemon.version.display=$(CUSTOM_VERSION) \
+    ro.doraemon.build_date=$(CUSTOM_BUILD_DATE) \
+    ro.doraemon.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
+    ro.doraemon.build_type=$(CUSTOM_BUILD_TYPE) \
+    ro.doraemon.fingerprint=$(ROM_FINGERPRINT)
 
 ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/secure/releasekey
